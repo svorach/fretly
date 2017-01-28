d25e52a9f336cf61fc195b888d7bc1ed44fd7d04
Shane Vorachek <svorach@gmail.com>
2017-01-28 18:18:53 -0500

    Removed invalid test.

--- test/components/Neck.spec.js
+++ test/components/Neck.spec.js
@@ -5,10 +5,4 @@ import { expect } from 'chai';
 import Neck from '../../src/components/neck/Neck.jsx';
 
 describe('<Neck />', function () {
-  it('Should contain a list of 6 strings', function() {
-    const wrapper = shallow(<Neck />);
-    const strings = wrapper.find('String');
-
-    expect(strings.nodes.length).to.equal(6);
-  });
 });
