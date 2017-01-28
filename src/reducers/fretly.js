40379d81fd3e529b5216ce45a085972117fabd07
Shane Vorachek <svorach@gmail.com>
2017-01-28 18:02:15 -0500

    Finished implmenting the root reducer and extraction of the redux store.

--- src/reducers/fretly.js
+++ src/reducers/fretly.js
@@ -4,7 +4,7 @@ import visibilityFilter from './visibilityFilter';
 import tuning from './tuning';
 import { combineReducers } from 'redux';
 
-export const reducer = combineReducers({
+export const fretly = combineReducers({
   tuning,
   activeScale,
   scales,
--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
 import { createStore } from 'redux';
-import { reducer } from './reducers/rootReducer';
+import { fretly } from './reducers/fretly';
 
-export const store = createStore(reducer);
+export const store = createStore(fretly);
